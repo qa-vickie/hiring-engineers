ff2ebf111c3b1e22ab902114df177ad94044144f
Vickie Groteboer <qa.vickie@gmail.com>
2016-05-13 22:10:00 -0400

    clean up

--- qa.vickie/checks.d/vickie_test.py
+++ qa.vickie/checks.d/vickie_test.py
@@ -8,11 +8,3 @@ class TestSupportRandomCheck(AgentCheck):
 	def check(self, nistance):
 		self.gauge('test.support.random', random.random())
 
-		aggregation_key = md5('testSupportRandom').hexdigest()
-
-		self.event({
-			'timestamp': int(time.time()),
-			'event_type': 'testSupportRandom_check',
-			'msg_title': 'TestSupportRandom event title',
-			'aggregation_key': aggregation_key 
-		})
